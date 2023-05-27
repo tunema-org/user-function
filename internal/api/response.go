1ab2ee99e866fea475e0869f93dd41ccbe15e3a0
HotPotatoC <juandotulung@gmail.com>
2023-05-27 07:54:08 +0700

    refactor: removed unused response methods

--- internal/api/response.go
+++ internal/api/response.go
@@ -1,38 +1,7 @@
 package api
 
 import (
-	"encoding/json"
-	"net/http"
-
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/gin-gonic/gin"
 )
 
 type M gin.H
-
-func JSON(status int, body any) (*events.APIGatewayProxyResponse, error) {
-	resp := events.APIGatewayProxyResponse{
-		Headers:    map[string]string{"Content-Type": "application/json"},
-		StatusCode: status,
-	}
-
-	jsonBody, _ := json.Marshal(body)
-	resp.Body = string(jsonBody)
-	return &resp, nil
-}
-
-func JSONMethodNotAllowed(allowed string) (*events.APIGatewayProxyResponse, error) {
-	return &events.APIGatewayProxyResponse{
-		Headers:    map[string]string{"Content-Type": "application/json"},
-		StatusCode: http.StatusMethodNotAllowed,
-		Body:       `{"message": "Method Not Allowed", "allowed_method": ` + allowed + `}`,
-	}, nil
-}
-
-func JSONNotFound() (*events.APIGatewayProxyResponse, error) {
-	return &events.APIGatewayProxyResponse{
-		Headers:    map[string]string{"Content-Type": "application/json"},
-		StatusCode: http.StatusNotFound,
-		Body:       `{"message": "Not Found"}`,
-	}, nil
-}
